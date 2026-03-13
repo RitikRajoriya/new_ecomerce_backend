65bfc7cdbdf649cc13ff48fd13686d223699e17b
RitikRajoriya <chamanrajoriya12@gmail.com>
2026-03-13 12:33:41 +0530

    ritik

--- models/Order.js
+++ models/Order.js
@@ -49,7 +49,7 @@ const orderSchema = new mongoose.Schema(
     paymentMethod: {
       type: String,
       required: true,
-      enum: ['card', 'paypal', 'bank_transfer'],
+      
     },
     paymentStatus: {
       type: String,
